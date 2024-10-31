d07af078c84abcd6206d72ff0958bd73817b4ce8
deshmukh-ajinkya <deshmukhasb@gmail.com>
2024-10-31 18:31:11 +0530

    feat: css improvement

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -1,8 +1,14 @@
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import React from 'react';
 
 function Dashboard(): React.ReactElement {
-  return <Typography variant="h4">Dashboard</Typography>;
+  return (
+    <Typography variant="h4">
+      <Box fontSize={24} component="a" href="https://github.com/deshmukh-ajinkya/reactbasekit">
+        Follow Readme From Github To Get Started
+      </Box>
+    </Typography>
+  );
 }
 
 export default Dashboard;
